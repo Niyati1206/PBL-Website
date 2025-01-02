eb7bca21844aa29cf6e5e47fbeadabdf815877cb
Vaishali <v.sirimalla@somaiya.edu>
2025-01-02 21:35:11 +0530

    registeration try

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 # Create your views here.
 def say_hello(request):
     return render(request,'home.html')
 
 def page2(request):
     return render(request,'page2.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Assuming 'home' is your home page URL
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to home or another page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')  # Redirect to home or another page after logout
+
+
+
--- storefront/settings.py
+++ storefront/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'playground'
 ]
 
+AUTHENTICATON_BACKENDS=[
+    'django.contrib.auth.backends.ModelBackend'
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
